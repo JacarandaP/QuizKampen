b5b2c7f02fc8c106045958e47befec17173c5d59
Jacaranda Perez <jacaranda.perez@gmail.com>
2020-11-22 15:57:24 +0100

    Game's logic is completed.

--- src/Serverside/PropertyReader.java
+++ src/Serverside/PropertyReader.java
@@ -17,8 +17,8 @@ public class PropertyReader {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        numberOfRounds = Integer.parseInt(p.getProperty("roundsInput", "4"));
-        numberOfQuestions = Integer.parseInt(p.getProperty("questionsInput", "3"));
+        numberOfRounds = Integer.parseInt(p.getProperty("roundsInput", "2"));
+        numberOfQuestions = Integer.parseInt(p.getProperty("questionsInput", "2"));
     }
 
     public int getNumberOfRounds() {
