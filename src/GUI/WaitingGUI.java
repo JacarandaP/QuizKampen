76a8c0b475044b44f19ce968d65171be6f89a236
Karl Danielsson <karl.danielsson@yh.nackademin.se>
2020-12-01 09:21:31 +0100

    Cleaned up WaitingGUI

--- src/GUI/WaitingGUI.java
+++ src/GUI/WaitingGUI.java
@@ -7,12 +7,9 @@ public class WaitingGUI extends JPanel {
     private JLabel txt = new JLabel();
     private JPanel waitingPanel = new JPanel(new GridBagLayout());
 
-
-
-    public WaitingGUI(String text)  {
-
+    public WaitingGUI(String text) {
         txt.setText(text);
-        waitingPanel.setPreferredSize(new Dimension(500,500));
+        waitingPanel.setPreferredSize(new Dimension(500, 500));
         waitingPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         waitingPanel.add(txt);
         add(waitingPanel);
